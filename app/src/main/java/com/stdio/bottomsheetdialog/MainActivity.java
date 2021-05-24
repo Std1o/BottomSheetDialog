e4681dc2f656bcb2896ecb97715cb1b3f8148c1e
Std1o <37378410+Std1o@users.noreply.github.com>
2021-05-24 17:21:34 +0300

    bottom sheet dialog

--- app/src/main/java/com/stdio/bottomsheetdialog/MainActivity.java
+++ app/src/main/java/com/stdio/bottomsheetdialog/MainActivity.java
@@ -3,12 +3,37 @@ package com.stdio.bottomsheetdialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomSheetDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                View view = getLayoutInflater().inflate(R.layout.fragment_bottom_sheet_dialog, null);
+
+                dialog = new BottomSheetDialog(this);
+                dialog.setContentView(view);
+                dialog.show();
+                break;
+            default:
+                Toast.makeText(this, String.valueOf(v.getId()), Toast.LENGTH_SHORT).show();
+                if (dialog != null) {
+                    dialog.cancel();
+                }
+                break;
+        }
+    }
+
 }
